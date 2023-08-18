eebf13f179c47b616be6fd229a9f2e85e095bc2b
Minh Duc Ngo <minhducngo85@gmail.com>
2023-08-18 18:12:17 +0200

    KAfka integration with SringBoot

---
+++ KafkaSpringBoot/src/main/java/com/minhduc/tuto/KafkaSpringBoot/KafkaSpringBootApplication.java
@@ -0,0 +1,46 @@
+package com.minhduc.tuto.KafkaSpringBoot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class KafkaSpringBootApplication implements ApplicationRunner {
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringBootApplication.class, args);
+	}
+
+	/**
+	 * to publish msg
+	 * 
+	 * @param msg
+	 */
+	public void sendMessage(String msg) {
+		kafkaTemplate.send("springboot-kafka", msg);
+	}
+
+	@KafkaListener(topics = "springboot-kafka", groupId = "group-spring-app")
+	public void listen(String message) {
+		System.out.println("Received Messasge in group (group-id): " + message);
+	}
+
+	/**
+	 * to send msg to kafka server after app started
+	 */
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("Send msg to kafka server");
+		for (int i = 0; i < 100; i++) {
+			sendMessage("Hi Welcome to Spring For Apache Kafka. Msg " + i);
+			Thread.sleep(1000);
+		}
+	}
+}
--- SimpleKafkaExample/src/main/java/com/minhduc/kafka/advanced/thread/ConsumerThreadDemo.java
+++ SimpleKafkaExample/src/main/java/com/minhduc/kafka/advanced/thread/ConsumerThreadDemo.java
@@ -18,79 +18,80 @@ import org.slf4j.LoggerFactory;
 
 public class ConsumerThreadDemo {
 
-    private static final Logger log = LoggerFactory.getLogger(ConsumerThreadDemo.class);
+	private static final Logger log = LoggerFactory.getLogger(ConsumerThreadDemo.class);
 
-    /**
-     * Running a Java Consumer in a separate thread allows you to perform other
-     * tasks in the main thread.
-     * 
-     * @param args
-     */
-    public static void main(String[] args) {
+	/**
+	 * Running a Java Consumer in a separate thread allows you to perform other
+	 * tasks in the main thread.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
 
-	ConsumerWorker worker = new ConsumerWorker();
-	new Thread(worker, "ConsumerWorkerThread").start();
+		ConsumerWorker worker = new ConsumerWorker();
+		new Thread(worker, "ConsumerWorkerThread").start();
 
-	// add the shutdown hook
-	Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+		// add the shutdown hook
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
 
-	    @Override
-	    public void run() {
-		try {
-		    worker.shutdown();
-		} catch (InterruptedException e) {
-		    log.error("Error shutting down consumer", e);
-		}
-	    }
-	}));
-    }
-
-    private static class ConsumerWorker implements Runnable {
-
-	private static final Logger log = LoggerFactory.getLogger(ConsumerWorker.class);
-	private CountDownLatch countDownLatch;
-	private Consumer<String, String> consumer;
-
-	@Override
-	public void run() {
-	    log.info("I'm Consumer Worker!");
-	    // wait until countdown = 0, than other clients will be executed
-	    countDownLatch = new CountDownLatch(1);
-	    final Properties properties = new Properties();
-
-	    // set consumer properties
-	    properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-	    properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-	    properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-	    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "my-kafka-thread-application");
-	    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-	    // init Kafka Consumer
-	    consumer = new KafkaConsumer<>(properties);
-	    consumer.subscribe(Collections.singleton("demo_java"));
-
-	    try {
-		while (true) {
-		    ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(100));
-		    for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
-			log.info("Getting consumer record key: '" + consumerRecord.key() + "', value: '" + consumerRecord.value() + "', partition: "
-			        + consumerRecord.partition() + " and offset: " + consumerRecord.offset() + " at "
-			        + new Date(consumerRecord.timestamp()));
-		    }
-		}
-	    } catch (WakeupException e) {
-		log.info("Consumer poll woke up");
-	    } finally {
-		consumer.close();
-		countDownLatch.countDown();
-	    }
+			@Override
+			public void run() {
+				try {
+					worker.shutdown();
+				} catch (InterruptedException e) {
+					log.error("Error shutting down consumer", e);
+				}
+			}
+		}));
 	}
 
-	void shutdown() throws InterruptedException {
-	    consumer.wakeup();
-	    countDownLatch.await();
-	    log.info("Consumer closed");
-	}
+	private static class ConsumerWorker implements Runnable {
 
-    }
+		private static final Logger log = LoggerFactory.getLogger(ConsumerWorker.class);
+		private CountDownLatch countDownLatch;
+		private Consumer<String, String> consumer;
+
+		@Override
+		public void run() {
+			log.info("I'm Consumer Worker!");
+			// wait until countdown = 0, than other clients will be executed
+			countDownLatch = new CountDownLatch(1);
+			final Properties properties = new Properties();
+
+			// set consumer properties
+			properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+			properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+			properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+			properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "my-kafka-thread-application");
+			properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+			// init Kafka Consumer
+			consumer = new KafkaConsumer<>(properties);
+			consumer.subscribe(Collections.singleton("demo_java"));
+
+			try {
+				while (true) {
+					ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(100));
+					for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+						log.info("Getting consumer record key: '" + consumerRecord.key() + "', value: '"
+								+ consumerRecord.value() + "', partition: " + consumerRecord.partition()
+								+ " and offset: " + consumerRecord.offset() + " at "
+								+ new Date(consumerRecord.timestamp()));
+					}
+				}
+			} catch (WakeupException e) {
+				log.info("Consumer poll woke up");
+			} finally {
+				consumer.close();
+				countDownLatch.countDown();
+			}
+		}
+
+		void shutdown() throws InterruptedException {
+			consumer.wakeup();
+			countDownLatch.await();
+			log.info("Consumer closed");
+		}
+
+	}
 }
